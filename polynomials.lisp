644be9c1c7acc623d55f3bcc9c94fd118227775b
William Woelffel <william.woelffel@gmail.com>
2016-11-21 20:48:14 +0100

    Polynomials in progress
    Division is being implemented, and should later be used to simplify
    the Reed-Solomon error correction generation.

--- polynomials.lisp
+++ polynomials.lisp
@@ -1,15 +1,15 @@
 (in-package :qr-generator)
 
 (defclass polynomial ()
-  ((real-coefs :initarg :real-coefs
-	       :reader real-coefs)
+  ((coefs :initarg :coefs
+	       :reader coefs)
    (degree :accessor degree
 	   :documentation "This is updated automatically and should not be manually set.")))
 
 (defmethod initialize-instance :after ((object polynomial) &key)
-  (with-slots (real-coefs degree) object
-    (setf real-coefs (remove-trailing-zeros real-coefs))
-    (setf degree (1- (length real-coefs)))))
+  (with-slots (coefs degree) object
+    (setf coefs (remove-trailing-zeros coefs))
+    (setf degree (1- (length coefs)))))
 
 (defun remove-trailing-zeros (list)
   (let ((result nil)
@@ -20,11 +20,16 @@
 	(push number result)))
     result))
 
-(defgeneric (setf real-coefs) (value poly))
+(defgeneric (setf coefs) (value poly))
 
-(defmethod (setf real-coefs) (value (poly polynomial))
-  (setf (slot-value poly 'real-coefs) (remove-trailing-zeros value))
-  (setf (degree poly) (1- (length value))))
+(defmethod (setf coefs) (value (poly polynomial))
+  (setf (slot-value poly 'coefs) (remove-trailing-zeros value))
+  (setf (degree poly) (1- (length value)))
+  poly)
+
+(defun x-power-n (n)
+  "Returns the `polynomial' object representing 1*x^n."
+  (make-instance 'polynomial :coefs (append (loop repeat n collect 0) '(1))))
 
 (defmethod print-object ((poly polynomial) stream)
   (flet ((merge-lists (l1 l2)
@@ -34,9 +39,9 @@
 	 (exponents (degree)
 	   (loop for i upto degree collect i)))
    (print-unreadable-object (poly stream :type t)
-     (with-slots (real-coefs degree) poly
-       (format t "~{~dx^~d~^ + ~}"
-	       (nreverse (merge-lists real-coefs (exponents degree))))))))
+     (with-slots (coefs degree) poly
+       (format stream "~{~dx^~d~^ + ~}"
+	       (nreverse (merge-lists coefs (exponents degree))))))))
 
 (defun nth-galois (n)
   "Return the number in the Galois field corresponding to 2 to the N"
@@ -52,11 +57,13 @@
   (reduce #'logxor (remove-if #'null numbers)))
 
 (defun galois-product (a b)
-  (nth-galois (mod (+ (galois-exponent a) (galois-exponent b)) 255)))
+  (if (or (zerop a) (zerop b))
+      0
+      (nth-galois (mod (+ (galois-exponent a) (galois-exponent b)) 255))))
 
 (defmethod add (poly-1 poly-2)
   (flet ((pad-polynomial (short-poly to-degree)
-	   (append (real-coefs short-poly)
+	   (append (coefs short-poly)
 		   (loop repeat (- to-degree (degree short-poly))
 			collect 0))))
     (let ((short-poly (if (<= (degree poly-1) (degree poly-2))
@@ -66,37 +73,55 @@
 			 poly-1
 			 poly-2)))
       (make-instance 'polynomial
-		     :real-coefs (mapcar #'galois-add (real-coefs long-poly)
+		     :coefs (mapcar #'galois-add (coefs long-poly)
 					 (pad-polynomial short-poly (degree long-poly)))))))
 
 (defgeneric multiply (poly-1 poly-2)
   (:documentation "Multiplication of two polynomials in the Galois field. The second one may be an integer."))
 
 (defmethod multiply ((poly polynomial) (constant integer))
-  (with-slots (real-coefs) poly
+  (with-slots (coefs) poly
     (make-instance
      'polynomial
-     :real-coefs (mapcar (lambda (x) (galois-product x constant)) real-coefs))))
+     :coefs (mapcar (lambda (x) (galois-product x constant)) coefs))))
 
 
 (defmethod multiply ((poly-1 polynomial) (poly-2 polynomial))
   (flet ((shift-degree (poly degree-shift)
 	   (unless (zerop degree-shift)
-	     (setf (real-coefs poly)
+	     (setf (coefs poly)
 		   (append (loop repeat degree-shift collect 0)
-			   (real-coefs poly))))
+			   (coefs poly))))
 	   poly))
-    (loop for coef in (real-coefs poly-1)
+    (loop for coef in (coefs poly-1)
        for degree-shift upfrom 0
        collect (shift-degree (multiply poly-2 coef) degree-shift) into intermediate-products
-	 finally (return (reduce #'add intermediate-products)))))
+       finally (return (reduce #'add intermediate-products)))))
+
+(defgeneric divide (poly-1 poly-2))
+
+(defmethod divide ((poly-1 polynomial) (poly-2 polynomial))
+  (let ((dividend (if (> (degree poly-1) (degree poly-2))
+		      poly-1
+		      poly-2))
+	(divisor (if (> (degree poly-1) (degree poly-2))
+		     poly-2
+		     poly-1)))
+    (loop 
+       for loop-dividend = dividend then (add loop-dividend intermediate-product)
+       for loop-divisor = (multiply divisor (x-power-n (- (degree dividend) (degree divisor))))
+       then (setf (coefs loop-divisor) (rest (coefs loop-divisor)))
+       for intermediate-product = (multiply loop-divisor (alexandria:last-elt (coefs loop-dividend)))
+       do (print loop-dividend)
+	 while (>= (degree loop-dividend) (degree divisor))
+       finally (return loop-dividend))))
 
 (defvar *generator-galois*
   (loop with result = (make-array 36)
      for counter upfrom 0
      for i across (subseq *log-antilog* 0 36)
-     for pol = (make-instance 'polynomial :real-coefs '(1 1))
-       then (multiply pol (make-instance 'polynomial :real-coefs (list i 1)))
+     for pol = (make-instance 'polynomial :coefs '(1 1))
+       then (multiply pol (make-instance 'polynomial :coefs (list i 1)))
      do (setf (aref result counter) pol)
        finally (return result)) 
   "Polynomials, in the Galois field, used as generators in the Reed-Solomon process.")
